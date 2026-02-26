8eb3c0948c2ee34bd5df7d85c9183e6c0dd069e5
Wesley Miranda <wesleyrod.br@gmail.com>
2026-02-26 18:18:29 -0300

    feat: create session class and votingsession enum

---
+++ src/main/java/com/teste/api/domain/session/Session.java
@@ -0,0 +1,49 @@
+package com.teste.api.domain.session;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import com.teste.api.domain.topic.Topic;
+
+@Table(name = "session")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Session {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "topic_id", nullable = false)
+    private Topic topic;
+
+    @Column(name = "opening_date", nullable = false, updatable = false)
+    private LocalDateTime openingDate;
+
+    @Column(name = "closing_date", nullable = false)
+    private LocalDateTime closingDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private VotingSessionStatus status;
+
+}
---
+++ src/main/java/com/teste/api/domain/session/VotingSessionStatus.java
@@ -0,0 +1,6 @@
+package com.teste.api.domain.session;
+
+public enum VotingSessionStatus {
+    OPEN,
+    CLOSED
+}
